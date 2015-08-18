f3a27261d96150d813676818af1bf70d20bddcff
Nurhashimah <nurhashimah77@gmail.com>
2015-08-18 16:06:10 +0800

    Fuel Transaction - validation added

--- app/models/fuel_transaction.rb
+++ app/models/fuel_transaction.rb
@@ -2,10 +2,18 @@ class FuelTransaction < ActiveRecord::Base
   belongs_to :fuel_unit_type, :class_name => "UnitType" , :foreign_key => "fuel_unit_type_id"
   belongs_to :fuel_type
   belongs_to :fuel_tank
-  belongs_to :vehicle #usage only
+  belongs_to :vehicle
+  validates_presence_of :amount
+  validate :fuel_type_must_match
 
   def self.by_type(transaction_type)
     where(transaction_type: transaction_type)
+  end
+  
+  def fuel_type_must_match
+    if fuel_type_id != fuel_tank.fuel_type_id
+      errors.add(:fuel_type_id, " must match with fuel type of Fuel Tank")
+    end
   end
   
 end
