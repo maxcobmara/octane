8fc44547da672ad4ff53532e6c898942cdc93e68
Nurhashimah <nurhashimah77@gmail.com>
2015-08-24 10:44:41 +0800

    Minor fixes for vessels related items

--- app/models/vessel_category.rb
+++ app/models/vessel_category.rb
@@ -2,6 +2,8 @@ class VesselCategory < ActiveRecord::Base
   has_many :vessels
   belongs_to :vessel_type
   
+  validates_presence_of :vessel_type_id, :short_name, :description
+  
   def type_category
     description+" ("+vessel_type.description+")"
   end
--- app/models/vessel_type.rb
+++ app/models/vessel_type.rb
@@ -1,4 +1,3 @@
 class VesselType < ActiveRecord::Base
-  #has_many :vessels
   has_many :vessel_categories
 end
